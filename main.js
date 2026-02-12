5ca56d39dd926883309ac93b41075e9517362207
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-12 18:35:16 +0100

    inserimento calcolo prezzo finale

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const formEl = document.querySelector("form")
 
 
 
+
 const nomeEl = document.getElementById("name")
 const carrozzaEl = document.getElementById("carrozza")
 const codiceEl = document.getElementById("codice")
@@ -23,10 +24,33 @@ formEl.addEventListener('submit', function(e){
 
     const numeroCarrozza = Math.floor(Math.random()* 9000)+1
     const cod = Math.floor(Math.random()* 9000)+1
+
+    const spesa_tot = kilometriValue * 0.21;
+    const sconto_1= (spesa_tot * 20) / 100;
+    const sconto_2=(spesa_tot * 40) / 100;
+    let prezzo = ""
+    if(ageValue === "Minorenne"){
+        prezzo = spesa_tot - sconto_1
+
+
+    }
+    else if(ageValue === "Maggiorenne"){
+        prezzo = spesa_tot - sconto_2
+
+    }
+    else{
+        prezzo = spesa_tot
+
+    }
     
     nomeEl.innerHTML = nomeValue
     carrozzaEl.innerHTML = numeroCarrozza
     codiceEl.innerHTML = cod
+    prezzoEl.innerHTML = prezzo.toFixed(2)
+
+
+
+
     
 
 
